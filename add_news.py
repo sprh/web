28395b617326122d978e4bf1368f36ffc2b31811
sprh.t <sprh.tt@gmail.com>
2019-03-24 21:12:20 +0300

    Создана шапка, разработан дизайн 1.0

--- add_news.py
+++ add_news.py
@@ -2,12 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField, StringField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email, Optional, Length
-from flask import render_template, redirect, Flask
+from flask import render_template, redirect, Flask, request
 from db import NewsModel, UsersModel, db
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 session = {}
+recent = '/login'
 
 
 class AddNewsForm(FlaskForm):
@@ -19,7 +21,6 @@ class AddNewsForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
     reg = SubmitField('Зарегестрироваться')
 
@@ -36,6 +37,8 @@ class RegistrationForm(FlaskForm):
 
 @app.route('/add_news', methods=['GET', 'POST'])
 def add_news():
+    global recent
+    recent = '/add_news'
     if 'username' not in session:
         return redirect('/login')
     form = AddNewsForm()
@@ -46,7 +49,7 @@ def add_news():
         nm.insert(title, content, session['user_id'])
         return redirect("/index")
     return render_template('add_news.html', title='Добавление новости',
-                           form=form, username=session['username'])
+                           form=form, username=session['username'], about_page='')
 
 
 @app.route('/delete_news/<int:news_id>', methods=['GET'])
@@ -61,12 +64,14 @@ def delete_news(news_id):
 @app.route('/')
 @app.route('/index')
 def index():
+    global recent
+    recent = '/index'
     if 'username' not in session:
         print(session)
         return redirect('/login')
     news = NewsModel(db.get_connection()).get_all(session['user_id'])
     return render_template('index.html', username=session['username'],
-                           news=news, title='Новости')
+                           news=news, title='Новости', about_page='')
 
 
 @app.route('/logout')
@@ -78,6 +83,8 @@ def logout():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global recent
+    recent = 'login'
     form = LoginForm()
     if form.reg.data:
         return redirect("/registration")
@@ -93,22 +100,46 @@ def login():
             return render_template('login.html', title='Авторизация', form=form,
                                    err='Проверьте правильность введенных данных')
         return redirect("/index")
-    return render_template('login.html', title='Авторизация', form=form)
+    return render_template('login.html', title='Авторизация', form=form,
+                           about_page='')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    global recent
+    recent = '/registration'
     form = RegistrationForm()
     if form.back.data:
         email = form.email
-        print(type(email))
         return redirect('/index')
     if form.validate_on_submit():
         user_name = form.username.data
         password = form.password.data
         email = form.email.data
         print(type(email))
-    return render_template('registration.html', title='Регистрация', form=form, err='')
+    return render_template('registration.html', title='Регистрация', form=form, err='',
+                           about_page='Расскажите нам немного о себе!')
+
+
+@app.route('/about_us')
+def about_us():
+    file = open('templates/about_us.txt', 'r')
+    text = file.readlines()
+    return render_template('about_us.html', title='О проекте', text=text, href=recent)
+
+
+@app.route('/contacts')
+def contacts():
+    with open("templates/contacts.json", "rt", encoding="utf8") as f:
+        text = json.loads(f.read())
+    return render_template('contacts.html', title='Контакты', href=recent, text=text)
+
+
+@app.route('/cooperation')
+def cooperation():
+    with open("templates/contacts.json", "rt", encoding="utf8") as f:
+        text = json.loads(f.read())
+    return render_template('cooperation.html', title='Сотрудничество', href=recent, text=text)
 
 
 if __name__ == '__main__':
